6fab7704db509b3ea19fb3d8a3da3b51c1c85c41
Tonatiuj Sánchez <tonsanzjimz@gmail.com>
2025-02-18 01:27:27 -0600

    Colores personalizados, añadidos

--- app/index.tsx
+++ app/index.tsx
@@ -4,13 +4,13 @@ import { View, Text } from 'react-native'
 const App = () => {
   return (
     <View className="mt-7 mx-2.5">
-      <Text className="text-slate-800 ml-5 text-4xl font-work-black">
+      <Text className="text-primary ml-5 text-4xl font-work-black">
         ¡Hola mundo - RN!
       </Text>
-      <Text className="text-slate-800 ml-5 text-3xl font-work-medium">
+      <Text className="text-secondary ml-5 text-3xl font-work-medium">
         ¡Hola mundo - RN!
       </Text>
-      <Text className="text-slate-800 ml-5 text-2xl font-work-light">
+      <Text className="text-tertiary ml-5 text-2xl font-work-light">
         ¡Hola mundo - RN!
       </Text>
     </View>
--- tailwind.config.js
+++ tailwind.config.js
@@ -8,11 +8,20 @@ module.exports = {
   presets: [require("nativewind/preset")],
   theme: {
     extend: {
+      colors: {
+        'primary': '#49129C',
+        'secondary': {
+          DEFAULT: '#B40086',
+          100: '#C51297',
+          200: '#831266',
+        },
+        'tertiary': '#EF2967'
+      },
       fontFamily: {
         'work-black': ['WorkSans-Black', 'sans-serif'],
         'work-light': ['WorkSans-Light', 'sans-serif'],
         'work-medium': ['WorkSans-Medium', 'sans-serif'],
-      }
+      },
     },
   },
   plugins: [],
