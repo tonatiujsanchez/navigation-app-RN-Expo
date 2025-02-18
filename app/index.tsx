a4e77e0ef3f2fbacc69ab75c9e2e77edae13d868
Tonatiuj Sánchez <tonsanzjimz@gmail.com>
2025-02-18 17:07:38 -0600

    Redirect del index al home, añadido

--- app/index.tsx
+++ app/index.tsx
@@ -1,19 +1,8 @@
-import { View, Text } from 'react-native'
-
+import { Redirect } from 'expo-router'
 
 const App = () => {
   return (
-    <View className="mt-7 mx-2.5">
-      <Text className="text-primary ml-5 text-4xl font-work-black">
-        ¡Hola mundo - RN!
-      </Text>
-      <Text className="text-secondary ml-5 text-3xl font-work-medium">
-        ¡Hola mundo - RN!
-      </Text>
-      <Text className="text-tertiary ml-5 text-2xl font-work-light">
-        ¡Hola mundo - RN!
-      </Text>
-    </View>
+    <Redirect href={'/home'} />
   )
 }
 
